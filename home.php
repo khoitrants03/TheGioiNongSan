491c29c607cef5244d4a9c08f0d2515b30c04019
khoitrants03 <trankhoi671@gmail.com>
2025-05-24 16:55:20 +0700

    khoi

--- home.php
+++ home.php
@@ -37,8 +37,6 @@ include 'components/add_cart.php';
 <body>
 
    <?php
-    
-   
     if (isset($_SESSION['phanquyen'])) {
         if ($_SESSION['phanquyen'] === 'nongdan') {
             require("components/user_header_nongdan.php");
